72c6a1a4c7b9609b56583e739bde79beea5026f5
Guikai <guikai_huang@outlook.com>
2022-09-25 23:19:16 -0500

    init the project

---
+++ src/guikai/Main.java
@@ -0,0 +1,12 @@
+package guikai;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(String arg : args){
+            System.out.println(arg);
+        }
+        System.out.println("success!");
+    }
+}
